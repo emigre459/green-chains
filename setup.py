0c989ef84c6d969f7ebcce07c86ba0fa05585beb
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2023-11-14 18:09:10 -0500

    Update src -> greenchains

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='greenchains',
     packages=find_packages(),
     version='0.1.0',
     description='A project for tracking and minimizing Scope 3 carbon emissions for supply chains',
     author='Dave Rench McCauley',
-    license='',
+    license='Apache v2',
 )
